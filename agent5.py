abd4306e74c393f1e99534107f2b08a7803ec509
Jon <jon@example.com>
2024-04-25 01:13:16 +0800

    update 2

--- agent5.py
+++ agent5.py
@@ -118,7 +118,7 @@ target_net = DQN(n_observations, n_actions).to(device)
 target_net.load_state_dict(policy_net.state_dict())
 
 optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
-memory = ReplayMemory(10000)
+memory = ReplayMemory(100000)
 
 
 steps_done = 0
